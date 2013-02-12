be906131ba80a086f44935c8ca8831f53d2abcdf
Jesse Luehrs <doy@tozt.net>
2013-02-12 17:00:32 -0600

    move this into its own file

---
+++ src/main/scala/app.scala
@@ -0,0 +1,22 @@
+import cake.Container
+
+object MyApp extends App {
+  val container1 = new Container
+  println("running container 1")
+  container1.application.run
+  println("done with container 1")
+
+  val container2 = new Container(logFileName = "other.log")
+  println("running container 2")
+  container2.application.run
+  println("done with container 2")
+
+  val container3 = new Container(
+    dsn      = "dbi:SQLite::memory:",
+    username = "",
+    password = ""
+  )
+  println("running container 3")
+  container3.application.run
+  println("done with container 3")
+}
--- src/main/scala/cake.scala
+++ src/main/scala/cake.scala
@@ -30,24 +30,3 @@ class Container (
   lazy val logger      = new Logger(logFileName)
   lazy val database    = Database.connect(dsn, username, password)
 }
-
-object MyApp extends App {
-  val container1 = new Container
-  println("running container 1")
-  container1.application.run
-  println("done with container 1")
-
-  val container2 = new Container(logFileName = "other.log")
-  println("running container 2")
-  container2.application.run
-  println("done with container 2")
-
-  val container3 = new Container(
-    dsn      = "dbi:SQLite::memory:",
-    username = "",
-    password = ""
-  )
-  println("running container 3")
-  container3.application.run
-  println("done with container 3")
-}
